258b465ec25aadeb7eddb766bceda586dafcc584
rahulcmr11 <41458947+rahulcmr11@users.noreply.github.com>
2023-07-17 11:21:37 +0530

    Update jenkins-demo-cicd.java

--- jenkins-demo-cicd.java
+++ jenkins-demo-cicd.java
@@ -20,9 +20,9 @@ node {
 		    
 	sh '''
 			whoami
-			 #sudo cp ./index.html /var/www/html/index.html
+			
 		echo "Files are republished"
-		         #sudo cp ./index.html C:/trainings/Jenkins_Demo/jenkins_training/jenkins-demo
+		        
       #sudo service apache2 restart
 		'''
 			
